81bea04d9d62e3efa59ead71a5b1a90eeaef64d5
Gail Bowen <gailbowen1@gmail.com>
2019-10-27 15:21:51 +0000

    Add EditView

---
+++ ZzaDashboard/Principal_Parts/PrincipalPartsEditView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ZzaDashboard.Principal_Parts
+{
+    public partial class PrincipalPartsEditView : UserControl
+    {
+        public PrincipalPartsEditView()
+        {
+            InitializeComponent();
+        }
+
+        public Guid CustomerId
+        {
+            get { return (Guid)GetValue(CustomerIdProperty); }
+            set { SetValue(CustomerIdProperty, value); }
+        }
+
+        public static readonly DependencyProperty CustomerIdProperty =
+            DependencyProperty.Register("CustomerId", typeof(Guid), 
+            typeof(PrincipalPartsEditView), new PropertyMetadata(Guid.Empty));
+    }
+}
